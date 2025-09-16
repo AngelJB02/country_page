36d1e804c87f2790bc9d76705800f47cad317113
ValeriaSanchezS <valeriasanchezs_@hotmail.com>
2025-09-16 13:38:18 -0500

    FRONT - Instructor

--- country_app/src/components/InstructorClases.jsx
+++ country_app/src/components/InstructorClases.jsx
@@ -7,6 +7,7 @@ import NoReservations from './instructor/NoReservations';
 import ReservaList from './instructor/ReservaList';
 import LogoutButton from './LogoutBoton';
 import useInstructor from '../hooks/useInstructor';
+import '../CSS/ReservaItem.css';
 
 const InstructorReservas = () => {
   const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
--- country_app/src/components/instructor/ReservaItem.jsx
+++ country_app/src/components/instructor/ReservaItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const ReservaItem = ({ reserva, updateAsistencia }) => {
 
   const getEstadoClass = (estado) => {
