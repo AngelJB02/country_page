7a33cb071962439c03c7b3701418ebdf1b54f3ec
Ulises Barragan <200300681@ucaribe.edu.mx>
2025-09-16 18:05:49 -0500

    correcion de localhost a produccion

--- country_app/src/hooks/useInstructor.js
+++ country_app/src/hooks/useInstructor.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = 'http://localhost:3001/api/instructor';
+const API_URL = 'https://country-page.onrender.com/api/instructor';
 
 const useInstructor = () => {
   const [reservas, setReservas] = useState([]);
